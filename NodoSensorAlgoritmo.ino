3724adc9699485d4ddcb570330ab2d5101fa0c47
Educaruzzo <30328949+Educaruzzo@users.noreply.github.com>
2017-07-25 19:23:04 -0300

    Update NodoSensorAlgoritmo.ino

--- NodoSensorAlgoritmo.ino
+++ NodoSensorAlgoritmo.ino
@@ -20,37 +20,8 @@ void loop()
   Roll = FunctionsPitchRoll(AcY, AcX, AcZ);
   Yaw = FunctionsPitchRoll(AcZ, AcY, AcX);
   
-  //Envia valor X do acelerometro para a serial
-  Serial.print("AcX = "); Serial.print(AcX);
-   
-  //Envia valor Y do acelerometro para a serial
-  Serial.print(" | AcY = "); Serial.print(AcY);
-   
-  //Envia valor Z do acelerometro para a serial
-  Serial.print(" | AcZ = "); Serial.print(AcZ);
-   
-  //Envia valor da temperatura para a serial
-  //Calcula a temperatura em graus Celsius
-  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);
-   
-  //Envia valor X do giroscopio para a serial
-  Serial.print(" | GyX = "); Serial.print(GyX);
-   
-  //Envia valor Y do giroscopio para a serial
-  Serial.print(" | GyY = "); Serial.print(GyY);
-   
-  //Envia valor Z do giroscopio para a serial
-  Serial.print(" | GyZ = "); Serial.println(GyZ);
-   
-  //Envia valor do Pitch para a serial
-  Serial.print(" | Pitch = "); Serial.print(Pitch);
-   
-  //Envia valor do Roll para a serial
-  Serial.print(" | Roll = "); Serial.print(Roll);
-   
-  //Envia valor do Yaw para a serial
-  Serial.print(" | Yaw = "); Serial.println(Yaw);
-   
+  ExibeValoresViaSerial(AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ, Pitch, Roll, Yaw);
+  
   //Aguarda 300 ms e reinicia o processo
   delay(800);
 }
@@ -92,4 +63,37 @@ int FunctionsPitchRoll(double A, double B, double C){
   Value = Value * 180/3.14;
   
   return (int) Value;
+}
+
+void ExibeValoresViaSerial(int AcX, int AcY, int AcZ, int Tmp, int GyX, int GyY, int GyZ, int Pitch, int Roll, int Yaw) {
+  //Envia valor X do acelerometro para a serial
+  Serial.print("AcX = "); Serial.print(AcX);
+   
+  //Envia valor Y do acelerometro para a serial
+  Serial.print(" | AcY = "); Serial.print(AcY);
+   
+  //Envia valor Z do acelerometro para a serial
+  Serial.print(" | AcZ = "); Serial.print(AcZ);
+   
+  //Envia valor da temperatura para a serial
+  //Calcula a temperatura em graus Celsius
+  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);
+   
+  //Envia valor X do giroscopio para a serial
+  Serial.print(" | GyX = "); Serial.print(GyX);
+   
+  //Envia valor Y do giroscopio para a serial
+  Serial.print(" | GyY = "); Serial.print(GyY);
+   
+  //Envia valor Z do giroscopio para a serial
+  Serial.print(" | GyZ = "); Serial.println(GyZ);
+   
+  //Envia valor do Pitch para a serial
+  Serial.print(" | Pitch = "); Serial.print(Pitch);
+   
+  //Envia valor do Roll para a serial
+  Serial.print(" | Roll = "); Serial.print(Roll);
+   
+  //Envia valor do Yaw para a serial
+  Serial.print(" | Yaw = "); Serial.println(Yaw);
 }
