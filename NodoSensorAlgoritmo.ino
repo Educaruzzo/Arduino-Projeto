1ae22819206b3043ba51d2d4081b742183323710
Educaruzzo <30328949+Educaruzzo@users.noreply.github.com>
2017-07-27 08:33:12 -0300

    Update NodoSensorAlgoritmo.ino

--- NodoSensorAlgoritmo.ino
+++ NodoSensorAlgoritmo.ino
@@ -28,20 +28,20 @@ void setup()
 
 void loop()
 {
-  CapturaValores(&AcX, &AcY, &AcZ, &Tmp, &GyX, &GyY, &GyZ);
+  CapturaValores();
 
-  Norma = sqrt(pow(AcX, 2) + pow(AcY, 2) + pow(AcZ, 2));
+  Norma = sqrt(square(AcX) + square(AcY) + square(AcZ));
   //Serial.print(" | Norma = "); Serial.println(Norma);
   
   Pitch = FunctionsPitchRoll(AcX, AcY, AcZ);
   Roll = FunctionsPitchRoll(AcY, AcX, AcZ);
   Yaw = FunctionsPitchRoll(AcZ, AcY, AcX);
   
-  ExibeValoresViaSerial(AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ, Pitch, Roll, Yaw, Estado);
+  ExibeValoresViaSerial();
   Movimento = (abs((AcX - antAcX) + (AcY - antAcY) + (AcZ - antAcZ)) >= LIMITE_OSCILACAO_MOVIMENTO);
-  Estado = VerificaEstado(Pitch, Roll, Yaw, Movimento);
+  Estado = VerificaEstado();
   
-  DetectaQueda(Norma, Estado);
+  DetectaQueda();
   
   antAcX = AcX;
   antAcY = AcY;
@@ -60,7 +60,7 @@ void InicializaMPU() {
   delay(1000);
 }
 
-void CapturaValores(int *AcX, int *AcY, int *AcZ, int *Tmp, int *GyX, int *GyY, int *GyZ) {
+void CapturaValores() {
   Wire.beginTransmission(MPU);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
@@ -69,13 +69,13 @@ void CapturaValores(int *AcX, int *AcY, int *AcZ, int *Tmp, int *GyX, int *GyY, 
   Wire.requestFrom(MPU,14,true);  
   
   //Armazena o valor dos sensores nas variaveis correspondentes
-  (*AcX)= Wire.read()<<8|Wire.read();  //0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
-  (*AcY)= Wire.read()<<8|Wire.read();  //0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  (*AcZ)= Wire.read()<<8|Wire.read();  //0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  (*Tmp)= Wire.read()<<8|Wire.read();  //0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  (*GyX)= Wire.read()<<8|Wire.read();  //0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  (*GyY)= Wire.read()<<8|Wire.read();  //0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  (*GyZ)= Wire.read()<<8|Wire.read();  //0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  AcX= Wire.read()<<8|Wire.read();  //0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+  AcY= Wire.read()<<8|Wire.read();  //0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ= Wire.read()<<8|Wire.read();  //0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp= Wire.read()<<8|Wire.read();  //0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX= Wire.read()<<8|Wire.read();  //0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY= Wire.read()<<8|Wire.read();  //0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ= Wire.read()<<8|Wire.read();  //0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 }
 
 int FunctionsPitchRoll(double A, double B, double C){
@@ -90,7 +90,7 @@ int FunctionsPitchRoll(double A, double B, double C){
   return (int) Value;
 }
 
-void ExibeValoresViaSerial(int AcX, int AcY, int AcZ, int Tmp, int GyX, int GyY, int GyZ, int Pitch, int Roll, int Yaw, int Estado) {
+void ExibeValoresViaSerial() {
   //Envia valor X do acelerometro para a serial
   Serial.print("AcX = "); Serial.print(AcX);
    
@@ -141,7 +141,7 @@ void ExibeValoresViaSerial(int AcX, int AcY, int AcZ, int Tmp, int GyX, int GyY,
   }
 }
 
-int VerificaEstado(int Pitch, int Roll, int Yaw, int Movimento) {
+int VerificaEstado() {
 
   if (abs(Pitch) >= 45) {
     if (Movimento) {
@@ -156,7 +156,7 @@ int VerificaEstado(int Pitch, int Roll, int Yaw, int Movimento) {
   return INDEFINIDO;
 }
 
-void DetectaQueda(int Norma, int Estado) {
+void DetectaQueda() {
 
   if (Norma > LIMITE_VARIACAO_QUEDA) {
     if (Estado != PARADO) {
